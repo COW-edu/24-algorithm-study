c94fefc93245d87509d1e4164be42ec1ebba502a
k-kbk <kkbk0077@gmail.com>
2024-08-21 20:44:15 +0900

    1730

---
+++ k-kbk/3주차/1730.py
@@ -0,0 +1,40 @@
+directions = {
+    "U": (-1, 0),
+    "D": (1, 0),
+    "L": (0, -1),
+    "R": (0, 1),
+}
+
+n = int(input())
+commands = list(input())
+
+points = [[chr(46)] * n for _ in range(n)]
+x, y = 0, 0
+prev = ""
+
+
+def draw(points, prev, command):
+    if command in ["U", "D"]:
+        points[y][x] = (
+            chr(43) if prev in ["L", "R"] or points[y][x] == chr(45) else chr(124)
+        )
+    if command in ["L", "R"]:
+        points[y][x] = (
+            chr(43) if prev in ["U", "D"] or points[y][x] == chr(124) else chr(45)
+        )
+
+
+for command in commands:
+    newX = x + directions[command][1]
+    newY = y + directions[command][0]
+    if 0 <= newX < n and 0 <= newY < n:
+        draw(points, prev, command)
+        x = newX
+        y = newY
+        prev = command
+
+
+draw(points, "", prev)
+
+for row in points:
+    print("".join(row))
