cf109eeb1c87738e819dcccfc863f94e706bffab
k-kbk <kkbk0077@gmail.com>
2024-09-04 13:41:10 +0900

    9012

---
+++ k-kbk/5주차/9012.py
@@ -0,0 +1,19 @@
+t = int(input())
+
+for _ in range(t):
+    line = input()
+    stack = []
+    
+    for char in line:
+        if len(stack) == 0:
+            stack.append(char)
+            continue
+        
+        last = stack[-1]
+        
+        if last == "(" and char == ")":
+            stack.pop()
+        else:
+            stack.append(char)
+
+    print("YES"  if len(stack) == 0 else "NO")
