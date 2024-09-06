1bfc65e9921a44a0b39b202a67eec087d96d4c3b
koseonje <kosunje1344@naver.com>
2024-09-06 22:44:34 +0900

    1620 문제풀이

---
+++ KoSeonJe/4주차/num_1620.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class num_1620 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int n = Integer.parseInt(st.nextToken());
+    int m = Integer.parseInt(st.nextToken());
+    Map<Integer, String> pocketmonFinder = new HashMap<>();
+    Map<String, Integer> indexFinder = new HashMap<>();
+    createPocket(br, n, pocketmonFinder, indexFinder);
+    StringBuilder sb = new StringBuilder();
+    printAnwser(br, m, pocketmonFinder, indexFinder, sb);
+    bw.write(sb.toString());
+    br.close();
+    bw.close();
+  }
+
+  private static void printAnwser(BufferedReader br, int m, Map<Integer, String> pocketmonFinder,
+      Map<String, Integer> indexFinder, StringBuilder sb) throws IOException {
+    for (int i = 0; i < m; i++) {
+      String problem = br.readLine();
+      if (isInteger(problem)) {
+        int temp = Integer.parseInt(problem);
+        sb.append(pocketmonFinder.get(temp)).append("\n");
+      } else {
+        sb.append(indexFinder.get(problem)).append("\n");
+      }
+    }
+  }
+
+  private static boolean isInteger(String next) {
+    try {
+      Integer.parseInt(next);
+      return true;
+    } catch (NumberFormatException exception) {
+      return false;
+    }
+  }
+
+  private static void createPocket(BufferedReader br, int n, Map<Integer, String> pocketmonFinder,
+      Map<String, Integer> indexFinder) throws IOException {
+    for (int i = 0; i < n; i++) {
+      String input = br.readLine();
+      pocketmonFinder.put(i + 1, input);
+      indexFinder.put(input, i + 1);
+    }
+  }
+}
