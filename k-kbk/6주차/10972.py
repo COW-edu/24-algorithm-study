5284700fddf0e14c8ea0be98a3bbb33eb67e4857
k-kbk <kkbk0077@gmail.com>
2024-09-15 15:51:10 +0900

    10972

--- k-kbk/6주차/10972.py
+++ k-kbk/6주차/10972.py
@@ -3,7 +3,6 @@ nums = list(map(int, input().split()))
 
 for i in range(n - 1, 0, -1):
     if nums[i - 1] < nums[i]:
-
         for j in range(n - 1, 0, -1):
             if nums[i - 1] < nums[j]:
                 nums[i - 1], nums[j] = nums[j], nums[i - 1]
