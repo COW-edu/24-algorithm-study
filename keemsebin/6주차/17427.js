f82de8230c66cce0f2850a3d3844e437632cccea
keemsebeen <sebin0580@gmail.com>
2024-09-17 14:49:48 +0900

    17427

---
+++ keemsebin/6주차/17427.js
@@ -0,0 +1,20 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  let N = Number(input[0]);
+  let sum = 0;
+
+  for (let i = 1; i <= N; i++) {
+    sum += Math.floor(N / i) * i;
+  }
+
+  console.log(sum);
+});
